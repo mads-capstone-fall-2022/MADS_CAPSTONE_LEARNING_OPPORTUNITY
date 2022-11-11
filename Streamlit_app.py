e11db6bf6da543852ab584f67fcd833e3b3c3bbd
MADS Capstone <mads.capstone.fall.2022@gmail.com>
2022-11-11 13:21:01 -0500

    added gitignore

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -20,3 +20,23 @@ title = "GDP for countries in Oceania"
 #test = gpd.read_file('schooldistrict_sy1314_tl15.shp')
 #chart = alt.Chart(test).mark_geoshape()
 #st.altair_chart(chart)
+
+from gsheetsdb import connect
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
