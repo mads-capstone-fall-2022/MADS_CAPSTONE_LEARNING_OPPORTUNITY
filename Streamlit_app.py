2ef17dc6bd38c6d6ffbd272f3c388d1fdb83f169
MADS Capstone <mads.capstone.fall.2022@gmail.com>
2022-11-16 06:48:54 -0500

    dfsd

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -31,8 +31,7 @@ seda_map_df  = pd.DataFrame(list(rows))
 fig = make_subplots(rows=1, cols=2)
 
 fig = px.scatter_mapbox(seda_map_df, lat="latitude", lon="longitude", hover_name="NAME", hover_data=["GEOID"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300,
-                        row=1, col=1)
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.update_layout(mapbox_bounds={"west": -180, "east": -50, "south": 20, "north": 90})
