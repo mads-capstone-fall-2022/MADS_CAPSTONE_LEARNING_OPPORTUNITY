44821fce1331b6f4dca032f9fba4b74cd0d80f59
MADS Capstone <mads.capstone.fall.2022@gmail.com>
2022-11-20 14:28:08 -0500

    dfd

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -32,11 +32,11 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 child_oppurtunity_df = pd.DataFrame(list(rows))
 
 #add filters
-v_segment = st.selectbox(
+v_segment = st.sidebar.selectbox(
      'Which segment would you like to select',
      ('Segment 1', 'Segment 2', 'Segment 3','Segment 4'))
 
-v_subject = st.selectbox(
+v_subject = st.sidebar.selectbox(
      'Which subject would you like to select',
      ('Math', 'Reading'))
 
@@ -47,7 +47,7 @@ v_year_choice = st.sidebar.slider(
 #filter the dataframe
 child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['year'] == v_year_choice]
 child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['Segment']==int(v_segment[-1])-1]
-    
+ child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['subject']==v_subject]   
 #st.set_page_config(layout="wide")
 #fig = make_subplots(rows=1, cols=2)
 
