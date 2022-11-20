1f07f0a4e0c65af96fe74f075b3404b194a7e4a8
MADS Capstone <mads.capstone.fall.2022@gmail.com>
2022-11-20 10:09:40 -0500

    added filters

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -23,15 +23,32 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-sheet_url = st.secrets["seda_map_file"]
+sheet_url = st.secrets["child_oppurtunity_input_file"] #st.secrets["seda_map_file"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
-#print(type(rows))
-seda_map_df  = pd.DataFrame(list(rows))
 
+
+#print(type(rows))
+#seda_map_df  = pd.DataFrame(list(rows))
+child_oppurtunity_df = pd.DataFrame(list(rows))
+
+#add filters
+v_segment = st.selectbox(
+     'Which segment would you like to select',
+     ('Segment 1', 'Segment 2', 'Segment 3','Segment 4'))
+v_year_choice = st.sidebar.slider(
+    'Year:', min_value=2010, max_value=2015, step=1, value=2015)
+
+
+#filter the dataframe
+child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['year'].isin(v_year_choice)]
+child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['segment'].isin(v_segment)]
+    
+st.set_page_config(layout="wide")
 fig = make_subplots(rows=1, cols=2)
 
-fig = px.scatter_mapbox(seda_map_df, lat="latitude", lon="longitude", hover_name="NAME", hover_data=["GEOID"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+#fig = px.scatter_mapbox(seda_map_df, lat="latitude", lon="longitude", hover_name="NAME", hover_data=["GEOID"],
+                    #    color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig = px.scatter_mapbox(data_frame=child_oppurtunity_index_data_df,lat='latitude', lon='longitude',color_continuous_scale = 'rdylgn', color='Segment',color_discrete_sequence=px.colors.qualitative.G10, size='cs_mn_all_abs' ,text='sedaleaname')#, hover_name='subject')
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.update_geos(fitbounds="locations")
