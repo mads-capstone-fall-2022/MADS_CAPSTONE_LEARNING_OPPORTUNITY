bf89b8658b82e269a60a0bb88cc071dfbd111102
Katie Andrews <mads.capstone.fall.2022@gmail.com>
2022-11-23 19:51:24 -0500

    updated citations 8

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -7,7 +7,7 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 
 st.set_page_config(layout="wide")
-Report, Dashboard = st.tabs(["Report Page", "Dashboard Page"])
+Report, Dashboard, Test_Page = st.tabs(["Report Page", "Dashboard Page", "Test Page"])
 Dashboard.title("Compare Achievment scores on the same Scale!")
 
 
@@ -30,7 +30,7 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 #print(type(rows))
 #seda_map_df  = pd.DataFrame(list(rows))
-child_oppurtunity_df = pd.DataFrame(list(rows))
+child_opportunity_df = pd.DataFrame(list(rows))
 
 
 #add filters
@@ -47,15 +47,15 @@ v_year_choice = Dashboard.slider(
 
 
 #filter the dataframe
-child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['year'] == v_year_choice]
-child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['Segment']==int(v_segment[-1])-1]
-child_oppurtunity_df = child_oppurtunity_df[child_oppurtunity_df['subject']==v_subject]   
+child_oppurtunity_df = child_opportunity_df[child_opportunity_df['year'] == v_year_choice]
+child_oppurtunity_df = child_opportunity_df[child_opportunity_df['Segment']==int(v_segment[-1])-1]
+child_oppurtunity_df = child_opportunity_df[child_opportunity_df['subject']==v_subject]   
 #st.set_page_config(layout="wide")
 #fig = make_subplots(rows=1, cols=2)
 
 #fig = px.scatter_mapbox(seda_map_df, lat="latitude", lon="longitude", hover_name="NAME", hover_data=["GEOID"],
                     #    color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig = px.scatter_mapbox(child_oppurtunity_df,lat='latitude', lon='longitude', zoom=3, height=300, hover_name='sedaleaname',color_continuous_scale = 'rdylgn', color='subject', size='cs_mn_all_abs' ,text='sedaleaname')#, hover_name='subject')
+fig = px.scatter_mapbox(child_opportunity_df,lat='latitude', lon='longitude', zoom=3, height=300, hover_name='sedaleaname',color_continuous_scale = 'rdylgn', color='subject', size='cs_mn_all_abs' ,text='sedaleaname')#, hover_name='subject')
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.update_geos(fitbounds="locations")
@@ -69,7 +69,7 @@ Report.header('Team Learning Opportunity Blog Post')
 Report.text('Jay Korrapati and Katie Andrews')
 Report.subheader('Introduction')
 
-Report.markdown('''When _school districts_ in the US are judged, it is usually by comparison to other districts.  Parents use ratings sites like GreatSchools - which uses test scores, graduation rates, and other data (GreatSchools.org, n.d.) - to compare schools when they are looking to move to a new area.  State governments use standardized test scores to rank schools and districts and identify struggling schools (Klein, 2015).  The standardized test scores used in both cases were designed at the state level in response to the 2001 No Child Left Behind federal law, which mandated that states establish tests for reading and math with at least 3 levels of scores: basic, proficient, and advanced (Colorado Department of Education, n.d.).  While much of NCLB has been amended since then, these tests are still used.  
+Report.markdown('''When school districts in the US are judged, it is usually by comparison to other districts.  Parents use ratings sites like GreatSchools - which uses test scores, graduation rates, and other data (GreatSchools.org, n.d.) - to compare schools when they are looking to move to a new area.  State governments use standardized test scores to rank schools and districts and identify struggling schools (Klein, 2015).  The standardized test scores used in both cases were designed at the state level in response to the 2001 No Child Left Behind federal law, which mandated that states establish tests for reading and math with at least 3 levels of scores: basic, proficient, and advanced (Colorado Department of Education, n.d.).  While much of NCLB has been amended since then, these tests are still used.  
 
 But are such direct comparisons between school districts fair, or even enlightening?  Since US schools are primarily funded at the local level, not state or federal, there is a wide variety in school financial expenditure (Semuels, 2016).  Also, communities may have different levels of non-financial resources supporting education.  The test scores themselves are not directly comparable, since each state has a different set of tests.
 
@@ -91,7 +91,7 @@ Report.subheader('Discussion')
 Report.markdown(f'''Learning from other states' educational successes (ref EPI report)''')
 
 Report.subheader('Citations')
-Report.markdown('''<p style="padding-left: 2em; text-indent: -2em;">Carnoy, M., García, E., & Khavenson, T. (2015, October 30). Bringing it back home:  Why state comparisons are more useful than international comparisons for improving U.S. education policy. Economic Policy Institute. https://www.epi.org/publication/bringing-it-back-home-why-state-comparisons-are-more-useful-than-international-comparisons-for-improving-u-s-education-policy/ </p>
+Report.markdown('''<p style="padding-left: 2em; text-indent: -2em;">Carnoy, M., García, E., & Khavenson, T. (2015, October 30). <em>Bringing it back home:  Why state comparisons are more useful than international comparisons for improving U.S. education policy.</em> Economic Policy Institute. <a href="https://www.epi.org/publication/bringing-it-back-home-why-state-comparisons-are-more-useful-than-international-comparisons-for-improving-u-s-education-policy/">https://www.epi.org/publication/bringing-it-back-home-why-state-comparisons-are-more-useful-than-international-comparisons-for-improving-u-s-education-policy/</a> </p>
 <p style="padding-left: 2em; text-indent: -2em;">Colorado Department of Education. (n.d.). Every Student Succeeds Act side-by-side. Retrieved October 27, 2022 from https://www.cde.state.co.us/fedprograms/nclbwaiveressasummary</p>
 <p style="padding-left: 2em; text-indent: -2em;">Diversitydatakids.org. (2022). Child Opportunity Index (Version 2.0). [Data set]. https://data.diversitydatakids.org/dataset/coi20-child-opportunity-index-2-0-database?_external=True</p>
 <p style="padding-left: 2em; text-indent: -2em;">Fahle, E. M., Chavez, B., Kalogrides, D., Shear, B. R., Reardon, S. F., & Ho, A. D. (2021). Stanford Education Data Archive: Technical Documentation (Version 4.1). http://purl.stanford.edu/db586ns4974</p>
@@ -100,6 +100,6 @@ Report.markdown('''<p style="padding-left: 2em; text-indent: -2em;">Carnoy, M., 
 <p style="padding-left: 2em; text-indent: -2em;">National Center for Education Statistics. (2015). School district geographic relationship files. [Data set]. https://nces.ed.gov/programs/edge/Geographic/RelationshipFiles</p>
 <p style="padding-left: 2em; text-indent: -2em;">Noelke, C., McArdle, N., Baek, M., Huntington, N., Huber, R., Hardy, E., & Acevedo-Garcia, D. (2020). Child Opportunity Index 2.0 Technical Documentation. http://diversitydatakids.org/research-library/research-brief/how-we-built-it</p>
 <p style="padding-left: 2em; text-indent: -2em;">Reardon, S. F., Ho, A. D., Shear, B. R., Fahle, E. M., Kalogrides, D., Jang, H., & Chavez, B. (2021). Stanford Education Data Archive (Version 4.1). [Data set]. Stanford University. http://purl.stanford.edu/db586ns4974</p>
-<p style="padding-left: 2em; text-indent: -2em;">Semuels, A. (2016, August 25). Good school, rich school; bad school, poor school: The inequality at the heart of America’s education system. The Atlantic. https://www.theatlantic.com/business/archive/2016/08/property-taxes-and-unequal-schools/497333/</p>
+<p style="padding-left: 2em; text-indent: -2em;">Semuels, A. (2016, August 25). Good school, rich school; bad school, poor school: The inequality at the heart of America's education system. The Atlantic. https://www.theatlantic.com/business/archive/2016/08/property-taxes-and-unequal-schools/497333/</p>
 <p style="padding-left: 2em; text-indent: -2em;">United States Census Bureau. (2010). 2010: DEC redistricting data (PL 94-171). [Data set]. https://data.census.gov/cedsci/table?q=Decennial%20Census%20population&g=0100000US%241400000&d=DEC%20Redistricting%20Data%20%28PL%2094-171%29&tid=DECENNIALPL2020.P1</p>
 ''', unsafe_allow_html=True)
